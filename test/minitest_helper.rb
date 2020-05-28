3bf27c9515f26531da4bcf273a0f018402efc8ac
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-05-28 09:37:38 +0900

    Removed needless test files

--- test/minitest_helper.rb
+++
@@ -1,5 +0,0 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'net/telnet'
-require 'net/telnet/version'
-
-require 'minitest/autorun'
--- test/test_net_telnet.rb
+++
@@ -1,7 +0,0 @@
-require 'minitest_helper'
-
-class TestNetTelnet < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Net::Telnet::VERSION
-  end
-end
